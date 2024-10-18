efa3ad58c0d5465e45a02e50d47464d47f366068
Manvir  Kaur <manvir-kaur3@myseneca.ca>
2024-10-18 18:35:54 -0400

    question 6

--- q6.py
+++ q6.py
@@ -2,11 +2,11 @@
 
 def reverse_string(input_string):
     reversed_str = ""  
-    for char in input_string:  # Loop through each character 
-        reversed_str = char + reversed_str  # Add each character to the beginni>
+    for char in input_string:   
+        reversed_str = char + reversed_str  
     return reversed_str
 
 if __name__ == '__main__':
     user_input = input("Enter a string: ")  
     result = reverse_string(user_input)  
-    print(f"The reversed string is: {result}")  # Print the result
+    print(f"The reversed string is: {result}")  
